c50e04fe7b3a78fdb1657c960e47c170f877c30c
Rummiya <rumibashirova@gmail.com>
2025-06-16 14:54:15 +0600

    fix(companies): wrap update and getAll in try/catch

--- controllers/company-controller.js
+++ controllers/company-controller.js
@@ -91,35 +91,39 @@ const CompanyController = {
 			filePath = req.file.path;
 		}
 
-		const company = await prisma.company.findUnique({
-			where: {
-				id,
-			},
-		});
+		try {
+			const company = await prisma.company.findUnique({
+				where: {
+					id,
+				},
+			});
 
-		if (!company) {
-			res.status(404).json({ error: 'Компания не найдена' });
+			if (!company) {
+				res.status(404).json({ error: 'Компания не найдена' });
+			}
+
+			const isOwner = await isUserCompanyOwner(userId, company.id);
+
+			if (!isOwner) {
+				return res.status(403).json({ error: 'Вы не владелец этой компании' });
+			}
+
+			const updatedCompany = await prisma.company.update({
+				where: {
+					id: company.id,
+				},
+				data: {
+					name,
+					logoUrl: filePath ? `/${filePath}` : undefined,
+					bio,
+					website,
+				},
+			});
+
+			res.json(updatedCompany);
+		} catch (error) {
+			res.status(500).json({ error: 'Internal Server Error' });
 		}
-
-		const isOwner = await isUserCompanyOwner(userId, company.id);
-
-		if (!isOwner) {
-			return res.status(403).json({ error: 'Вы не владелец этой компании' });
-		}
-
-		const updatedCompany = await prisma.company.update({
-			where: {
-				id: company.id,
-			},
-			data: {
-				name,
-				logoUrl: filePath ? `/${filePath}` : undefined,
-				bio,
-				website,
-			},
-		});
-
-		res.json(updatedCompany);
 	},
 	getAllCompanies: async (req, res) => {
 		const { name, followed } = req.query;
@@ -141,19 +145,23 @@ const CompanyController = {
 			};
 		}
 
-		const companies = await prisma.company.findMany({
-			where: filters,
-			include: {
-				_count: {
-					select: {
-						events: true,
-						followers: true,
+		try {
+			const companies = await prisma.company.findMany({
+				where: filters,
+				include: {
+					_count: {
+						select: {
+							events: true,
+							followers: true,
+						},
 					},
 				},
-			},
-		});
+			});
 
-		res.json(companies);
+			res.json(companies);
+		} catch (error) {
+			res.status(500).json({ error: 'Internal Server Error' });
+		}
 	},
 };
 
