30faa63a7c841f9707d2146951d088c8a7817e73
Rummiya <rumibashirova@gmail.com>
2025-06-17 13:08:25 +0600

    check: check errors

--- controllers/follow-controller.js
+++ controllers/follow-controller.js
@@ -2,26 +2,26 @@ const { prisma } = require('../prisma/prisma-client');
 
 const FollowController = {
 	followUser: async (req, res) => {
-		const { followingId } = req.body;
+		const { companyId } = req.body;
 		const userId = req.user.userId;
 
-		if (followingId === userId) {
+		if (companyId === userId) {
 			return res.status(500).json({ error: 'Нельзя подписаться на себя' });
 		}
 
 		try {
-			const existingFollow = await prisma.follow.findFirst({
-				where: { AND: [{ followerId: userId }, { followingId }] },
+			const existingFollow = await prisma.companyFollower.findFirst({
+				where: { userId, companyId },
 			});
 
 			if (existingFollow) {
 				res.status(400).json({ error: 'Вы уже подписаны' });
 			}
 
-			await prisma.follow.create({
+			await prisma.companyFollower.create({
 				data: {
-					follower: { connect: { id: userId } },
-					following: { connect: { id: followingId } },
+					user: { connect: { id: userId } },
+					company: { connect: { id: companyId } },
 				},
 			});
 
@@ -32,19 +32,19 @@ const FollowController = {
 		}
 	},
 	unfollowUser: async (req, res) => {
-		const { followingId } = req.body;
+		const { companyId } = req.body;
 		const userId = req.user.userId;
 
 		try {
-			const follows = await prisma.follow.findFirst({
-				where: { AND: [{ followerId: userId }, { followingId }] },
+			const follows = await prisma.companyFollower.findFirst({
+				where: { userId, companyId },
 			});
 
 			if (!follows) {
 				return res.status(404).json({ error: 'Подписка не найдена' });
 			}
 
-			await prisma.follow.delete({
+			await prisma.companyFollower.delete({
 				where: { id: follows.id },
 			});
 
