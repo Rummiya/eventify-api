b2275df817261fdfe0384548edd79f818efec801
Rummiya <rumibashirova@gmail.com>
2025-06-17 13:15:06 +0600

    fix(auth): import prisma

--- midddleware/auth.js
+++ midddleware/auth.js
@@ -1,23 +1,34 @@
 const jwt = require('jsonwebtoken');
+const { prisma } = require('../prisma/prisma-client');
 
-const authenticateToken = (req, res, next) => {
+const authenticateToken = async (req, res, next) => {
 	const authHeader = req.headers['authorization'];
-
 	const token = authHeader && authHeader.split(' ')[1];
 
 	if (!token) {
-		return res.status(401).json({ error: 'Unauthorized' });
+		return res.status(401).json({ error: 'Вы не зарегистрированы' });
 	}
 
-	jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
-		if (err) {
-			return res.status(403).json({ error: 'Invalid token' });
+	try {
+		const decoded = jwt.verify(token, process.env.SECRET_KEY);
+
+		// Проверка, существует ли пользователь в базе
+		const user = await prisma.user.findUnique({
+			where: { id: decoded.userId },
+		});
+
+		if (!user) {
+			return res.status(401).json({ error: 'Пользователь не найден' });
 		}
 
-		req.user = user;
-
+		req.user = decoded;
 		next();
-	});
+	} catch (err) {
+		console.error('JWT error:', err.message);
+		return res
+			.status(403)
+			.json({ error: 'Недействительный или просроченный токен' });
+	}
 };
 
 module.exports = authenticateToken;
