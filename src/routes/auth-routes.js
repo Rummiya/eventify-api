0df341d6e59ed614e0a6f1b3c55d7e428c82cfb3
Rummiya <rumibashirova@gmail.com>
2025-06-19 09:42:53 +0600

    feat(auth): add docs to auth routes

--- src/routes/auth-routes.js
+++ src/routes/auth-routes.js
@@ -3,5 +3,62 @@ import { AuthController } from '../controllers/index.js';
 
 export const authRouter = Router();
 
+/**
+ * @swagger
+ * /api/auth/login:
+ *   post:
+ *     summary: Логин пользователя
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: test@mail.com
+ *               password:
+ *                 type: string
+ *                 example: qwerty123
+ *     responses:
+ *       200:
+ *         description: Успешная авторизация
+ *       401:
+ *         description: Неверные данные
+ */
 authRouter.post('/auth/login', AuthController.login);
+
+/**
+ * @swagger
+ * /api/auth/register:
+ *   post:
+ *     summary: Регистрация пользователя
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: test@mail.com
+ *               password:
+ *                 type: string
+ *                 example: qwerty123
+ *     responses:
+ *       200:
+ *         description: Успешная регистрация
+ *       401:
+ *         description: Неверные данные
+ */
 authRouter.post('/auth/register', AuthController.register);
