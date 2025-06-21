c1ef3257683ac3ed9cadc4e30d41e670236d538f
Rummiya <rumibashirova@gmail.com>
2025-06-21 22:51:08 +0600

    fix(user): make fields optional to update user schema

--- src/modules/user/user-schema.js
+++ src/modules/user/user-schema.js
@@ -1,6 +1,10 @@
 import { z } from 'zod';
 
 export const userSchema = z.object({
-	email: z.string().trim().email({ message: 'Некорректный email' }),
-	name: z.string().trim().min(2, 'Имя должно быть минимум 2 символа'),
+	email: z.string().trim().email({ message: 'Некорректный email' }).optional(),
+	name: z
+		.string()
+		.trim()
+		.min(2, 'Имя должно быть минимум 2 символа')
+		.optional(),
 });
