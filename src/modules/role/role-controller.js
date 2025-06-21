78523dfe9d770afcc6c27360101759037302b82b
Rummiya <rumibashirova@gmail.com>
2025-06-21 23:48:59 +0600

    feat(role): add create role controller

---
+++ src/modules/role/role-controller.js
@@ -0,0 +1,23 @@
+import { prisma } from '../../prisma/prisma-client.js';
+
+export const RoleController = {
+	createRole: async (req, res) => {
+		try {
+			const { value } = req.body;
+
+			const existingRole = await prisma.role.findUnique({
+				where: { value },
+			});
+
+			if (existingRole) {
+				return res.status(400).json({ error: 'Такая роль уже существует' });
+			}
+
+			const role = await prisma.role.create({ data: { value } });
+
+			res.json({ data: role, message: 'Роль успешно создана!' });
+		} catch (error) {
+			res.status(500).json({ error: 'Ошибка при создании роли' });
+		}
+	},
+};
