75e5daf8287f0dff9454f9598fe33526d849dad8
Rummiya <rumibashirova@gmail.com>
2025-06-21 15:35:37 +0600

    fix(auth): add trim validate to auth schemas

--- src/schemas/auth-schema.js
+++ src/schemas/auth-schema.js
@@ -1,12 +1,12 @@
 import { z } from 'zod';
 
 export const registerSchema = z.object({
-	email: z.string().email({ message: 'Некорректный email' }),
-	password: z.string().min(6, 'Пароль должен быть минимум 6 символов'),
-	name: z.string().min(2, 'Имя должно быть минимум 2 символа'),
+	email: z.string().trim().email({ message: 'Некорректный email' }),
+	password: z.string().trim().min(6, 'Пароль должен быть минимум 6 символов'),
+	name: z.string().trim().min(2, 'Имя должно быть минимум 2 символа'),
 });
 
 export const loginSchema = z.object({
-	email: z.string().email({ message: 'Некорректный email' }),
-	password: z.string().min(1, 'Пароль обязателен'),
+	email: z.string().trim().email({ message: 'Некорректный email' }),
+	password: z.string().trim().min(1, 'Пароль обязателен'),
 });
